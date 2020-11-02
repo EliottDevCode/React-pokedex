7054b18639253f2a35c5c8f22b9e64f4e424f625
Laetitia Gcv <lgoncalves@sfbx.io>
2020-11-02 19:09:59 +0100

    boxes pokemon and styling main page

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+import './App.css';
+import NavBar from "./components/layout/NavBar";
+import Dashboard from "./components/layout/Dashboard";
+
+function App() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+          <NavBar /> <div className='container'> <Dashboard /> </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
