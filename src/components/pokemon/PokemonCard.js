6be18c05225f772ec4cbbd01b216b3da9853de3e
Laetitia Gcv <lgoncalves@sfbx.io>
2020-12-03 18:58:09 +0100

    app finished

--- src/components/pokemon/PokemonCard.js
+++ src/components/pokemon/PokemonCard.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Link} from 'react-router-dom';
 
 import styled from "styled-components";
 
@@ -16,6 +17,23 @@ transition: all 0.3s cubic-bezier(0.25, 0.8, .25, 1);
 &:hover {
     box-shadow: 0 14px 28px rgba(0, 0, 0, 0.25), 0 10px rgba(0, 0, 0, 0.22);
  }
+ -moz-user-select: none;
+ -website-user-select: none;
+ user-select: none;
+ -o-user-select: none;
+`;
+
+const StyledLink = styled(Link)`
+
+text-decoration: none;
+color: black;
+&:focus,
+&:hover,
+&:visited,
+&:link,
+&:active {
+    text-decoration:none;
+}
 `;
 
 class PokemonCard extends Component {
@@ -43,38 +61,42 @@ class PokemonCard extends Component {
 
         return (
             <div className={"col-md-3 col-sm-6 mb-5"}>
-                <Card className="card">
-                    <h5 className={'card-header'}> {this.state.pokemonIndex} </h5>
-                    {this.state.imageLoading ? (
-                        <img src={spinner} style={{width: '5em', height: '5em'}} className="card-img-top rounded mx-auto d-block mt-2"/>
-                    ) : null}
-                    <Sprite
-                        className={"card-img-top rounded mx-auto mt-2"}
+                <StyledLink to={`pokemon/${this.state.pokemonIndex}`}>
+                    <Card className="card">
+                        <h5 className={'card-header'}> {this.state.pokemonIndex} </h5>
+                        {this.state.imageLoading ? (
+                            <img src={spinner} style={{width: '5em', height: '5em'}}
+                                 className="card-img-top rounded mx-auto d-block mt-2"/>
+                        ) : null}
+                        <Sprite
+                            className={"card-img-top rounded mx-auto mt-2"}
                             onLoad={() => this.setState({imageLoading: false})}
                             onError={() => this.setState({toManyRequests: true})}
                             src={this.state.imageUrl}
                             style={
-                                this.state.toManyRequests ? { display: "none"} :
-                                this.state.imageLoading ? null : {display: "block"}
+                                this.state.toManyRequests ? {display: "none"} :
+                                    this.state.imageLoading ? null : {display: "block"}
                             }
-                    />
+                        />
 
-                    {this.state.toManyRequests ? (
-                        <h6 className={"mx-auto"}>
-                            <span className={"badge badge-danger mt-2"}> Too Many Request</span>
-                        </h6>
-                    ) : null}
-                    <div className="card-body mx auto">
-                        <h6 className="card-title "> {this.state.name
-                            .toLocaleLowerCase()
-                            .split(' ')
-                            .map(
-                                letter => letter.charAt(0).toUpperCase() + letter.substring(1))
-                            .join(' ')}
-                        </h6>
-                    </div>
-                </Card>
+                        {this.state.toManyRequests ? (
+                            <h6 className={"mx-auto"}>
+                                <span className={"badge badge-danger mt-2"}> Too Many Request</span>
+                            </h6>
+                        ) : null}
+                        <div className="card-body mx auto">
+                            <h6 className="card-title "> {this.state.name
+                                .toLocaleLowerCase()
+                                .split(' ')
+                                .map(
+                                    letter => letter.charAt(0).toUpperCase() + letter.substring(1))
+                                .join(' ')}
+                            </h6>
+                        </div>
+                    </Card>
+                </StyledLink>
             </div>
+
         );
     }
 }
